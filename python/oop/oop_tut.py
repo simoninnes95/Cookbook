4bba3afe5edbe8adbea58f16c4d47ab3d8ca927f
simon innes <innessimon8@gmail.com>
2023-07-04 14:47:30 +0200

    update

---
+++ python/oop/oop_tut.py
@@ -0,0 +1,214 @@
+
+# SOLID OOP Tutorial
+# Single-responsibility principle (SRP)
+# Open–closed principle (OCP)
+# Liskov substitution principle (LSP)
+# Interface segregation principle (ISP)
+# Dependency inversion principle (DIP)
+
+# https://realpython.com/solid-principles-python/
+
+# 1. SRP
+
+# file_manager_srp.py
+
+from pathlib import Path
+from zipfile import ZipFile
+
+class FileManager:
+    def __init__(self, filename):
+        self.path = Path(filename)
+
+    def read(self, encoding="utf-8"):
+        return self.path.read_text(encoding)
+
+    def write(self, data, encoding="utf-8"):
+        self.path.write_text(data, encoding)
+
+class ZipFileManager:
+    def __init__(self, filename):
+        self.path = Path(filename)
+
+    def compress(self):
+        with ZipFile(self.path.with_suffix(".zip"), mode="w") as archive:
+            archive.write(self.path)
+
+    def decompress(self):
+        with ZipFile(self.path.with_suffix(".zip"), mode="r") as archive:
+            archive.extractall()
+
+# 2. OCP
+
+# shapes_ocp.py
+
+from abc import ABC, abstractmethod
+from math import pi
+
+class Shape(ABC):
+    def __init__(self, shape_type):
+        self.shape_type = shape_type
+
+    @abstractmethod
+    def calculate_area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        super().__init__("circle")
+        self.radius = radius
+
+    def calculate_area(self):
+        return pi * self.radius**2
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        super().__init__("rectangle")
+        self.width = width
+        self.height = height
+
+    def calculate_area(self):
+        return self.width * self.height
+
+class Square(Shape):
+    def __init__(self, side):
+        super().__init__("square")
+        self.side = side
+
+    def calculate_area(self):
+        return self.side**2
+
+# 3. LSP
+
+# shapes_lsp.py
+
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def calculate_area(self):
+        return self.width * self.height
+    
+class Square(Rectangle):
+    def __init__(self, side):
+        super().__init__(side, side)
+
+    def __setattr__(self, key, value):
+        super().__setattr__(key, value)
+        if key in ("width", "height"):
+            self.__dict__["width"] = value
+            self.__dict__["height"] = value
+
+# 4. ISP
+
+# printers_isp.py
+
+from abc import ABC, abstractmethod
+
+class Printer(ABC):
+    @abstractmethod
+    def print(self, document):
+        pass
+
+    @abstractmethod
+    def fax(self, document):
+        pass
+
+    @abstractmethod
+    def scan(self, document):
+        pass
+
+class OldPrinter(Printer):
+    def print(self, document):
+        print(f"Printing {document} in black and white...")
+
+    def fax(self, document):
+        raise NotImplementedError("Fax functionality not supported")
+
+    def scan(self, document):
+        raise NotImplementedError("Scan functionality not supported")
+
+class ModernPrinter(Printer):
+    def print(self, document):
+        print(f"Printing {document} in color...")
+
+    def fax(self, document):
+        print(f"Faxing {document}...")
+
+    def scan(self, document):
+        print(f"Scanning {document}...")
+
+
+# printers_isp.py
+
+from abc import ABC, abstractmethod
+
+class Printer(ABC):
+    @abstractmethod
+    def print(self, document):
+        pass
+
+class Fax(ABC):
+    @abstractmethod
+    def fax(self, document):
+        pass
+
+class Scanner(ABC):
+    @abstractmethod
+    def scan(self, document):
+        pass
+
+class OldPrinter(Printer):
+    def print(self, document):
+        print(f"Printing {document} in black and white...")
+
+class NewPrinter(Printer, Fax, Scanner):
+    def print(self, document):
+        print(f"Printing {document} in color...")
+
+    def fax(self, document):
+        print(f"Faxing {document}...")
+
+    def scan(self, document):
+        print(f"Scanning {document}...")
+
+5. DIP
+
+# app_dip.py
+
+class FrontEnd:
+    def __init__(self, back_end):
+        self.back_end = back_end
+
+    def display_data(self):
+        data = self.back_end.get_data_from_database()
+        print("Display data:", data)
+
+class BackEnd:
+    def get_data_from_database(self):
+        return "Data from the database"
+
+# app_dip.py
+
+from abc import ABC, abstractmethod
+
+class FrontEnd:
+    def __init__(self, data_source):
+        self.data_source = data_source
+
+    def display_data(self):
+        data = self.data_source.get_data()
+        print("Display data:", data)
+
+class DataSource(ABC):
+    @abstractmethod
+    def get_data(self):
+        pass
+
+class Database(DataSource):
+    def get_data(self):
+        return "Data from the database"
+
+class API(DataSource):
+    def get_data(self):
+        return "Data from the API"
